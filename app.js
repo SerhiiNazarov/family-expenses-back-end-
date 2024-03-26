bf2fce8fdf580d5e01fcd2609f0bce43e0aa7c93
SerhiiNazarov <algazira89@gmail.com>
2024-03-26 22:12:00 +0200

    fix app

--- app.js
+++ app.js
@@ -14,17 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use("/api/users", usersRouter);
-
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
